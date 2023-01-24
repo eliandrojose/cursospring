9c3e9c4a02611baeb461e3ee5a70ac2eb31a720b
eliandro <eliandrojose25@gmail.com>
2023-01-24 16:46:53 -0300

    Testanto a primeira classe de dominio

--- src/main/java/com/eliandro/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/eliandro/cursospring/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.eliandro.cursospring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eliandro.cursospring.domain.Categorias;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categorias> listar() {
+		
+		Categorias cat1 = new Categorias(1, "Informática"); 
+		Categorias cat2 = new Categorias(2, "Escritório" );
+		
+		List<Categorias> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 	
 
